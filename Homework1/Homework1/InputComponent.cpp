762d1c7108c7484671233c78bb3e84e430a94857
ReolSt <roxman98@naver.com>
2022-04-27 06:53:34 +0900

    [ADD]: Add InputComponent

--- Homework1/Homework1/InputComponent.cpp
+++ Homework1/Homework1/InputComponent.cpp
@@ -1,1 +1,57 @@
 #include "InputComponent.h"
+
+namespace Engine
+{
+	InputComponent::InputComponent(Actor* owner, const std::string& name) : Component(owner, name)
+	{
+		m_keyHandlers[EventType::KeyDown] = std::unordered_map<std::string, KeyEventCallback>();
+		m_keyHandlers[EventType::KeyUp] = std::unordered_map<std::string, KeyEventCallback>();
+
+		m_mouseHandlers[EventType::MouseDown] = std::unordered_map<std::string, MouseEventCallback>();
+		m_mouseHandlers[EventType::MouseUp] = std::unordered_map<std::string, MouseEventCallback>();
+		m_mouseHandlers[EventType::MouseWheel] = std::unordered_map<std::string, MouseEventCallback>();
+		m_mouseHandlers[EventType::MouseMove] = std::unordered_map<std::string, MouseEventCallback>();
+		m_mouseHandlers[EventType::MouseHover] = std::unordered_map<std::string, MouseEventCallback>();
+		m_mouseHandlers[EventType::MouseLeave] = std::unordered_map<std::string, MouseEventCallback>();
+	}
+
+	inline void InputComponent::addKeyHandler(EventType& eventType, KeyEventCallback& callback)
+	{
+		m_keyHandlers[eventType][callback.getId()] = callback;
+	}
+
+	inline void InputComponent::removeKeyHandler(EventType& eventType, KeyEventCallback& callback)
+	{
+		m_keyHandlers[eventType].erase(callback.getId());
+	}
+
+	inline void InputComponent::addMouseHandler(EventType& eventType, MouseEventCallback& callback)
+	{
+		m_mouseHandlers[eventType][callback.getId()] = callback;
+	}
+
+	inline void InputComponent::removeMouseHandler(EventType& eventType, MouseEventCallback& callback)
+	{
+		m_mouseHandlers[eventType][callback.getId()] = callback;
+	}
+
+	void InputComponent::onKeyEvent(KeyEvent* evt)
+	{
+		for (auto& pair : m_keyHandlers[evt->getType()])
+		{
+			auto& callback = pair.second;
+			callback(evt);
+		}
+	}
+
+	void InputComponent::onMouseEvent(MouseEvent* evt)
+	{
+		for (auto& pair : m_mouseHandlers[evt->getType()])
+		{
+			auto& callback = pair.second;
+			callback(evt);
+		}
+	}
+
+}
+
--- Homework1/Homework1/InputComponent.h
+++ Homework1/Homework1/InputComponent.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include "Component.h"
+#include "Event.h"
+#include "EventCallback.h"
+
+#include <unordered_map>
+#include <functional>
 
 namespace Engine
 {
@@ -10,6 +15,35 @@ namespace Engine
 		// ----------------------------------------------------------------------
 		// Consturctor, Destructor
 		// ----------------------------------------------------------------------
+
+		InputComponent(Actor* owner, const std::string& name);
+
+	public:
+		// ----------------------------------------------------------------------
+		// Event Handler Management
+		// ----------------------------------------------------------------------
+
+		void addKeyHandler(EventType& eventType, KeyEventCallback& callback);
+		void removeKeyHandler(EventType& eventType, KeyEventCallback& callback);
+
+		void addMouseHandler(EventType& eventType, MouseEventCallback& callback);
+		void removeMouseHandler(EventType& eventType, MouseEventCallback& callback);
+
+	protected:
+		// ----------------------------------------------------------------------
+		// Event Listeners
+		// ----------------------------------------------------------------------
+
+		void onKeyEvent(KeyEvent* evt);
+		void onMouseEvent(MouseEvent* evt);
+
+	private:
+		// ----------------------------------------------------------------------
+		// Private Member Variable
+		// ----------------------------------------------------------------------
+
+		std::unordered_map<EventType, std::unordered_map<std::string, KeyEventCallback>> m_keyHandlers;
+		std::unordered_map<EventType, std::unordered_map<std::string, MouseEventCallback>> m_mouseHandlers;
 	};
 }
 
