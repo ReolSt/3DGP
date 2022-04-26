e46ecc1af0dd4f15eb358f199833723350bd84d0
ReolSt <roxman98@naver.com>
2022-04-27 06:54:02 +0900

    [UPDATE]: Update world

--- Homework1/Homework1/World.cpp
+++ Homework1/Homework1/World.cpp
@@ -1,1 +1,148 @@
 #include "World.h"
+#include "Actor.h"
+#include "Component.h"
+
+#include <utility>
+
+namespace Engine
+{
+    World::World()
+    {
+    }
+
+    World::~World()
+    {
+    }
+
+    Actor* World::getActorById(const std::string& id)
+    {
+        for (auto pair : m_actors)
+        {
+            Actor* actor = pair.second;
+            if (actor->getId() == id)
+            {
+                return actor;
+            }
+        }
+
+        return nullptr;
+    }
+
+    std::vector<Actor*>&& World::getActorsByTypeId(size_t typeId)
+    {
+        std::vector<Actor*> container;
+
+        for (auto pair : m_actors)
+        {
+            Actor* actor = pair.second;
+            if (actor->getTypeId() == typeId)
+            {
+                container.push_back(actor);
+            }
+        }
+
+        return std::move(container);
+    }
+
+    std::vector<Actor*>&& World::getActorsByTypeName(const std::string& typeName)
+    {
+        std::vector<Actor*> container;
+
+        for (auto pair : m_actors)
+        {
+            Actor* actor = pair.second;
+            if (actor->getTypeName() == typeName)
+            {
+                container.push_back(actor);
+            }
+        }
+
+        return std::move(container);
+    }
+
+    void World::addActor(Actor* actor)
+    {
+        assert(actor != nullptr);
+
+        std::string actorId = actor->getId();
+        m_actors[actorId] = actor;
+    }
+
+    void World::removeActor(Actor* actor)
+    {
+        assert(actor != nullptr);
+
+        std::string actorId = actor->getId();
+        m_actors.erase(actorId);
+    }
+
+    Component* World::getRegisteredComponentById(const std::string& id)
+    {
+        for (const auto& pair : m_registeredComponents)
+        {
+            Component* component = pair.second;
+            if (component->getId() == id)
+            {
+                return component;
+            }
+        }
+
+        return nullptr;
+    }
+
+    std::vector<Component*>&& World::getRegisteredComponentsByTypeId(size_t typeId)
+    {
+        std::vector<Component*> container;
+
+        for (const auto& pair : m_registeredComponents)
+        {
+            Component* component = pair.second;
+            if (component->getTypeId() == typeId)
+            {
+                container.push_back(component);
+            }
+        }
+
+        return std::move(container);
+    }
+
+    std::vector<Component*>&& World::getRegisteredComponentsByTypeName(const std::string& typeName)
+    {
+        std::vector<Component*> container;
+
+        for (const auto& pair : m_registeredComponents)
+        {
+            Component* component = pair.second;
+            if (component->getTypeName() == typeName)
+            {
+                container.push_back(component);
+            }
+        }
+
+        return std::move(container);
+    }
+
+    void World::registerComponent(Component* component)
+    {
+        assert(component != nullptr);
+
+        std::string componentId = component->getId();
+        m_registeredComponents[componentId] = component;
+    }
+
+    void World::unRegisterComponent(Component* component)
+    {
+        assert(component != nullptr);       
+
+        std::string componentId = component->getId();
+        m_registeredComponents.erase(componentId);
+    }
+
+    void World::update()
+    {
+    }
+
+    void World::updateComponents()
+    {
+    }
+}
--- Homework1/Homework1/World.h
+++ Homework1/Homework1/World.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <vector>
+#include <unordered_map>
 #include <string>
-
-#include "BasicTypes.h"
 
 namespace Engine
 {
@@ -12,38 +10,40 @@ namespace Engine
 
 	class World
 	{
+	public:
 		// ----------------------------------------------------------------------
 		// Constructor, Destructor
 		// ----------------------------------------------------------------------
 		World();
 		virtual ~World();
 
+	public:
 		// ----------------------------------------------------------------------
 		// Public Member Method
 		// ----------------------------------------------------------------------
 
-		std::vector<Actor*>& getActors();
-		std::vector<Actor*>& getActorById();
+		Actor* getActorById(const std::string& id);
+		std::vector<Actor*>&& getActorsByTypeId(size_t typeId);
+		std::vector<Actor*>&& getActorsByTypeName(const std::string& typeName);
 
-		std::vector<Component*> getRegisteredComponents();
-		std::vector<Component*> getRegisteredComponentById(ObjectId id);
-		std::vector<Component*> getRegisteredComponentByTypeId(size_t typeId);
-		std::vector<Component*> getRegisteredComponentByTypeName(const std::string& typeName);
+		void addActor(Actor* actor);
+		void removeActor(Actor* actor);
+
+		Component* getRegisteredComponentById(const std::string& id);
+		std::vector<Component*>&& getRegisteredComponentsByTypeId(size_t typeId);
+		std::vector<Component*>&& getRegisteredComponentsByTypeName(const std::string& typeName);
+
+		void registerComponent(Component* component);
+		void unRegisterComponent(Component* component);
 
 		void update();
 		void updateComponents();
-
-		void render();
-
-		void addActor();
-		void registerComponent();
-
 	private:
 		// ----------------------------------------------------------------------
 		// Private Member Variable
 		// ----------------------------------------------------------------------
 
-		std::vector<Actor*> m_actors;
-		std::vector<Component*> m_registeredComponents;
+		std::unordered_map<std::string, Actor*> m_actors;
+		std::unordered_map<std::string, Component*> m_registeredComponents;
 	};
 }
